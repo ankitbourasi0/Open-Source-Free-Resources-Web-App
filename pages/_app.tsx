34651bf38a73f1aac0491e338f9e61064cadfe1e
Ankit Bourasi <ankitbourasi0@gmail.com>
2024-02-21 14:53:37 +0530

    placed google ads code snippet for verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,21 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
 import Layout from "layouts/Layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main>
+      <Script id="Adsense-id" data-ad-client="ca-pub-987************676"
+        async strategy="afterInteractive"
+        onError={(e) => { console.error('Script failed to load', e) }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <ThemeProvider attribute="class">
+
         <Analytics />
         <Layout>
-        <Component {...pageProps} />
+          <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
     </main>
